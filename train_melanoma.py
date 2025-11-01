b7aeccfa05c05fde90f0bd3e01ac4f68cedfe17c
DanaMariaN <ndana420@gmail.com>
2025-10-31 19:28:32 -0600

    codigo

--- train_melanoma.py
+++ train_melanoma.py
@@ -1,2 +1,49 @@
-print("Hola")
-print("Hola")
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, f1_score, recall_score, precision_score, roc_curve, auc, confusion_matrix
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+
+
+ruta_imagenes = r"d:\PROYECTO_MELANOMA\train"
+ruta_csv = r"d:\PROYECTO_MELANOMA\ISIC_2020_Training_GroundTruth_v2.csv"
+output_dir = r"d:\PROYECTO_MELANOMA\results_resnet"
+os.makedirs(output_dir, exist_ok=True)
+plots_dir = os.path.join(output_dir, "plots")
+os.makedirs(plots_dir, exist_ok=True)
+
+# CARGA Y LIMPIEZA CSV
+df = pd.read_csv(ruta_csv)
+df_simple = df[["image_name", "target"]].rename(columns={"target": "label"})
+df_simple.to_csv(os.path.join(output_dir, "metadata.csv"), index=False)
+print("Archivo metadata.csv creado con", len(df_simple), "registros.")
+
+df = df_simple.copy()
+df['image_path'] = df['image_name'].apply(lambda x: os.path.join(ruta_imagenes, x + ".jpg"))
+df = df[df['image_path'].apply(os.path.exists)]
+print("Imágenes encontradas:", df.shape[0])
+
+
+#DIVISION 70/15/15
+train_df, temp_df = train_test_split(df, test_size=0.3, stratify=df['label'], random_state=42)
+val_df, test_df = train_test_split(temp_df, test_size=0.5, stratify=temp_df['label'], random_state=42)
+print(f"Train: {len(train_df)}, Val: {len(val_df)}, Test: {len(test_df)}")
+
+#GENERADORES
+IMG_SIZE = (224, 224)
+train_gen = ImageDataGenerator(rescale=1./255, rotation_range=15, zoom_range=0.2,
+                               horizontal_flip=True, vertical_flip=True)
+val_test_gen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_gen.flow_from_dataframe(train_df, x_col='image_path', y_col='label',
+                                           target_size=IMG_SIZE, class_mode='raw', batch_size=32)
+val_data = val_test_gen.flow_from_dataframe(val_df, x_col='image_path', y_col='label',
+                                            target_size=IMG_SIZE, class_mode='raw', batch_size=32)
+test_data = val_test_gen.flow_from_dataframe(test_df, x_col='image_path', y_col='label',
+                                             target_size=IMG_SIZE, class_mode='raw', batch_size=32, shuffle=False)
